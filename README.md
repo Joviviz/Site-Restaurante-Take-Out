606ad2666d956f758b9236014de07f55ecb86e23
João Pedro Issmael Vieira <joao.pedrov@dinfor.uniceub>
2025-06-03 09:33:48 -0300

    Main.py 20%, role -> cargo

--- README.md
+++ README.md
@@ -10,3 +10,9 @@ Foto de Perfil | Registro de Aluno (RA) | Nome |
 | <img src='https://avatars.githubusercontent.com/u/101208535?v=4' width="80" height="80"> | 22252263 | João Pedro I. Vieira |
 | <img src='https://avatars.githubusercontent.com/u/111282811?v=4' width="80" height="80"> | 22250196 | Eric Gomes Barreto | 
 | <img src='https://avatars.githubusercontent.com/u/110557881?v=4' width="80" height="80"> | 22250316 | Leonardo Chung Bezerra | 
+
+
+## Instalando Dependências no venv
+```
+pip install flask flask-cors mysql-connector-python
+```
--- app/main.py
+++ app/main.py
@@ -7,5 +7,47 @@ CORS(app)
 
 def get_db_connection():
     return mysql.connector.connect(
-        
+        host = "localhost"
+        user = "root"
+        password = "" # Here password
+        database = "takeout_db"
     )
+
+# Usuário
+@app.route('/usuarios/criar', methods = ['POST'])
+def criar_usuario():
+    data = request.get_json()
+    nome = data.get('nome')
+    email = data.get('email')
+    senha = data.get('senha')
+    cargo = data.get('cargo')
+
+    con = get_db_connection()
+    cursor = con.cursor()
+    
+    query = 'INSERT INTO Usuario (nome, email, senha, cargo) VALUES (%s %s %s %s)'
+    cursor.execute(query, (nome, email, senha, cargo))
+    con.commit()
+    con.close()
+    return jsonify({'message': f'Usuário {nome} criado com sucesso!'}), 201
+
+@app.route('/usuarios/login', methods = ['POST'])
+def login_usuario():
+    data = request.get_json()
+    email = data.get('email')
+    senha = data.get('senha')
+
+    con = get_db_connection()
+    cursor = con.cursor()
+
+    query = 'SELECT id, nome, role FROM Usuario WHERER email = %s AND senha = %s'
+    cursor.execute(query,(email, senha))
+    usuario = cursor.fetchone()
+    con.close()
+
+    if usuario:
+        return jsonify({'message': 'Login bem-sucedido!', 'usuario':{'id': usuario[0], 'nome': usuario[1], 'cargo': usuario[2]}}), 200
+    else:
+        return jsonify({'message': 'Credenciais Inválidas.'}), 401
+    
+# Item Cardapio
