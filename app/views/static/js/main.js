f44ad48e4cda6e70a9d62d8c0b40947dbe043913
Joviviz <101208535+Joviviz@users.noreply.github.com>
2025-06-10 16:06:23 -0300

    Website massive style change -> black white, font, navbar, display

--- app/views/static/js/main.js
+++ app/views/static/js/main.js
@@ -15,62 +15,9 @@ angular.module('takeOutApp').config(['$routeProvider',
             when('/menu', {
                 templateUrl: '/views/menu.html'
             }).
+            when('/test', {
+                templateUrl: '/views/test.html'
+            }).
             otherwise('/home')
         }
     ]);
-
-
-// Controller 
-
-angular.module('takeOutApp').controller('AuthController', ['$scope', '$http', 
-    function($scope, $http) {
-        
-        $scope.registro = {
-            nome: '',
-            email: '',
-            senha: '',
-            role: 'CLIENTE' 
-        };
-        
-        $scope.login = {
-            email: '',
-            senha: ''
-        };
-        
-        // Mensagens de feedback
-        $scope.mensagem = '';
-        $scope.erro = '';
-        
-        // Função para registrar usuário
-        $scope.registrar = function() {
-            $http.post('/usuarios/criar', $scope.registro)
-                .then(function(response) {
-                    $scope.mensagem = response.data.message;
-                    $scope.erro = '';
-                    // Redireciona para login após registro
-                    window.location.hash = '#!/login';
-                })
-                .catch(function(error) {
-                    $scope.erro = error.data.message || 'Erro ao registrar usuário';
-                    $scope.mensagem = '';
-                });
-        };
-        
-        // Função para fazer login
-        $scope.fazerLogin = function() {
-            $http.post('/usuarios/login', $scope.login)
-                .then(function(response) {
-                    $scope.mensagem = response.data.message;
-                    $scope.erro = '';
-                    // Armazena os dados do usuário (simples, sem usar localStorage/sessionStorage)
-                    $scope.usuarioLogado = response.data.usuario;
-                    // Redireciona para home após login
-                    window.location.hash = '#!/home';
-                })
-                .catch(function(error) {
-                    $scope.erro = error.data.message || 'Credenciais inválidas';
-                    $scope.mensagem = '';
-                });
-        };
-    }
-]);
